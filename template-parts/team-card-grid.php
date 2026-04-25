2a761c131aa26e55240a61e5b1caaa5ab916c081
ArturoValv <art.valverde.563@gmail.com>
2026-04-24 22:41:04 -0600

    feat: finished team card grid

--- template-parts/team-card-grid.php
+++ template-parts/team-card-grid.php
@@ -9,11 +9,13 @@ if (!defined('ABSPATH')) {
         <a href="<?= $args['link_url'] ?>" target="<?= $args['link_target'] ?>" class="team-card-grid__wrapper" aria-label="<?= esc_attr($args['name']) ?>">
             <?php
             if (isset($args['picture']) && $args['picture'] && $args['picture'] !== '') {
+                $bg_item_url = get_template_directory_uri() . '\assets\img\thumbnail-bg.webp';
                 img_print_picture_tag(img: $args["picture"], max_size: "cover-mobile", min_size: "featured-small", classes: "team-card-grid__pic");
             } else {
                 include get_template_directory() . '/assets/icons/icon-file-image.svg';
             }
             ?>
+            <img data-src="<?= $bg_item_url ?>" alt="thumbnail wrapper" class="team-card-grid__pic-wrapper lazy-image">
 
             <div class="team-card-grid__inner tx-center">
 
